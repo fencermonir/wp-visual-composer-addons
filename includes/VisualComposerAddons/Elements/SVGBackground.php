b7754fa8ac87ca2a33a82b83465ec1bc2e9644c1
Mehdi Lahlou <mehdi.lahlou@free.fr>
2016-11-21 13:27:28 +0100

    add: add SVG background functionality first draft

---
+++ includes/VisualComposerAddons/Elements/SVGBackground.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Visual composer svg background element.
+ *
+ * @package visual-composer-addons
+ * @author  Mehdi Lahlou <mehdi.lahlou@free.fr>
+ * @since   1.0.0
+ */
+
+namespace MedFreeman\WP\VisualComposerAddons\Elements;
+
+use MedFreeman\WP\VisualComposerAddons\AbstractVCElement;
+
+/**
+ * Visual composer SVG background element class.
+ */
+class SVGBackground extends AbstractVCElement {
+
+	/**
+	 * Register visual composer element.
+	 *
+	 * @return void
+	 */
+	function vc_integration() {
+		$group      = 'Background SVG';
+		$shortcode  = 'vc_row';
+
+		/* Intro */
+		vc_add_param( $shortcode, array(
+			'type'        => 'checkbox',
+			'heading'     => 'Background SVG',
+			'param_name'  => 'svg_bg',
+			'value'       => array(
+								'Use background vector graphics' => 'true',
+							 ),
+			'description' => 'Responsive vector background.',
+			'group'       => $group,
+		) );
+
+		/* === Video Files === */
+
+		/* SVG URL */
+		vc_add_param( $shortcode, array(
+			'type'        => 'media',
+			'heading'     => 'SVG URL',
+			'param_name'  => 'svg_bg_id',
+			'mime'        => 'image/svg+xml',
+			'value'       => '',
+			'description' => 'Select or upload SVG file.',
+			'group'       => $group,
+			'dependency'  => array(
+								'element' => 'svg_bg',
+								'value'   => array( 'true' ),
+							 ),
+		) );
+	}
+
+	/**
+	 * Register visual composer element shortcode.
+	 * This element doesn't need this function since it only extends vc rows.
+	 *
+	 * @param array           $attrs shortcode attributes.
+	 * @param (string | null) $content shortcode contents.
+	 *
+	 * @return void
+	 */
+	function shortcode( $attrs, $content = null ) {
+
+	}
+
+	/**
+	 * Register visual composer element style.
+	 *
+	 * @return void
+	 */
+	function enqueue_scripts() {
+		wp_register_style( 'vc_svg_background', VCADDONS_URL . '/assets/css/Elements/SVGBackground.css' );
+		wp_enqueue_style( 'vc_svg_background' );
+	}
+}
---
+++ includes/VisualComposerAddons/Fields/MediaSelector.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Visual composer media selector field.
+ *
+ * @package visual-composer-addons
+ * @author  Mehdi Lahlou <mehdi.lahlou@free.fr>
+ * @since   1.0.0
+ */
+
+namespace MedFreeman\WP\VisualComposerAddons\Fields;
+
+use MedFreeman\WP\VisualComposerAddons\AbstractVCField;
+
+/**
+ * Visual composer Media selector field class.
+ */
+class MediaSelector extends AbstractVCField {
+
+	/**
+	 * Register visual composer field.
+	 *
+	 * @return void
+	 */
+	function vc_integration() {
+		vc_add_shortcode_param( 'media' , array( $this, 'media_field_output' ), VCADDONS_URL . '/assets/js/src/Fields/admin-media-selector.js' );
+	}
+
+	/**
+	 * Visual composer field output.
+	 *
+	 * @param  array  $settings VC Field settings.
+	 * @param  string $value    VC Field value.
+	 *
+	 * @return field output
+	 */
+	public function media_field_output( $settings, $value ) {
+		$dependency = vc_generate_dependencies_attributes( $settings );
+		$param_name = isset( $settings['param_name'] ) ? $settings['param_name'] : '';
+		$mime = isset( $settings['mime'] ) ? $settings['mime'] : '';
+		if ( is_array( $mime ) ) {
+			$mime = implode( ',', $mime );
+		}
+		$output = '<button type="button" class="button wpb-media-input-button" data-mime-type="' . esc_attr( $mime ) . '" style="float:right; margin-top: 3px;">Browse Media</button>';
+		$output .= '<input
+					type="text"
+					class="wpb-media-input-field"
+					value="' . esc_attr( wp_get_attachment_url( $value ) ) . '"
+					style="width: calc(100% - 125px);"
+					/>';
+		$output .= '<input
+			type="hidden"
+			class="wpb_vc_param_value wpb-media-id-field"
+			name="' . esc_attr( $param_name ) . '"
+			value="' . esc_attr( $value ) . '"
+			/>';
+
+		return $output;
+	}
+}
