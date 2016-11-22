85460861f7278d00c8c5d2ff39a5e485c636fbeb
Mehdi Lahlou <mehdi.lahlou@free.fr>
2016-11-22 15:59:00 +0100

    add: add number field type for offsets

---
+++ includes/VisualComposerAddons/Fields/Number.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Visual composer Number field.
+ *
+ * @package visual-composer-addons
+ * @author  Mehdi Lahlou <mehdi.lahlou@free.fr>
+ * @since   1.0.0
+ */
+
+namespace MedFreeman\WP\VisualComposerAddons\Fields;
+
+use MedFreeman\WP\VisualComposerAddons\AbstractVCField;
+
+/**
+ * Visual composer Number field class.
+ */
+class Number extends AbstractVCField {
+
+	/**
+	 * Register visual composer field.
+	 *
+	 * @return void
+	 */
+	function vc_integration() {
+		vc_add_shortcode_param( 'number' , array( $this, 'number_field_output' ) );
+	}
+
+	/**
+	 * Visual composer field output.
+	 *
+	 * @param  array  $settings VC Field settings.
+	 * @param  string $value    VC Field value.
+	 *
+	 * @return field output
+	 */
+	public function number_field_output( $settings, $value ) {
+		$dependency = vc_generate_dependencies_attributes( $settings );
+		$param_name = isset( $settings['param_name'] ) ? $settings['param_name'] : '';
+		$min = isset( $settings['min'] ) ? $settings['min'] : '';
+		$max = isset( $settings['max'] ) ? $settings['max'] : '';
+		$output = '<input
+					type="number"
+					min="' . esc_attr( $min ) . '"
+					max="' . esc_attr( $max ) . '"
+					class="wpb_vc_param_value wpb-number-input"
+					name="' . esc_attr( $param_name ) . '"
+					value="' . esc_attr( $value ) . '"
+					/>';
+		return $output;
+	}
+}
