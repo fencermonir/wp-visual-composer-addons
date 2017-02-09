71f1d793a53c1a0387cf9d5bbfc51ffe61b179bf
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-02-09 20:04:58 +0100

    change: remove color parameter, to allow white text to be readable

--- assets/js/src/Elements/CustomizableText.js
+++ assets/js/src/Elements/CustomizableText.js
@@ -1,6 +1,6 @@
 (function ($) {
 	window.VcCustomizableTextView = vc.shortcode_view.extend({
-		template: _.template('<div class="wpb__customizable__text" style="font-size:<%= params.size %>px;font-weight:<%= params.weight %>;color:<%= params.color %>;"><%= params.content %></div>'),
+		template: _.template('<div class="wpb__customizable__text" style="font-size:<%= params.size %>px;font-weight:<%= params.weight %>;"><%= params.content %></div>'),
 		// Render method called after element is added( cloned ), and on first initialisation
 		render: function () {
 			window.VcCustomizableTextView.__super__.render.call(this); //make sure to call __super__. To execute logic fron inherited view. That way you can extend original logic. Otherwise, you will fully rewrite what VC will do at this event
